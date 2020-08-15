242c37e2b5b33b24bda5c43a9efe6c83ac62dde8
jackhasakeyboard <jackhasakeyboard@gmail.com>
2020-08-14 23:10:37 -0600

    Make viewpoint in projection evenly distributed on every axis

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ int main() {
 	// matrix
 	glm::mat4
 		proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f),
-		view = glm::lookAt(glm::vec3(4, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)),
+		view = glm::lookAt(glm::vec3(3, 3, 3), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0)),
 		model = glm::mat4(1.0f);
 
 	// shader
